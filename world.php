8d5a1f830b70b8b65fe34b6cb5cf8b46bfd8afb7
Jamaal Henry <jamaalhenry30@gmail.com>
2021-11-09 16:27:23 -0500

    Edited countries to display in table format when a search is made

--- world.php
+++ world.php
@@ -5,6 +5,9 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
+$count = 0;
+
+
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
@@ -12,8 +15,28 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+
+<table> 
+<thead> 
+    <tr>
+      <th><?= 'Name'; ?></th>
+      <th><?= 'Continent'; ?></th>
+      <th><?= 'Independence'; ?></th>
+      <th><?= 'Head of State'; ?></th>
+    </tr>
+  </thead>
+<tbody>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+      <tr>
+        <td><?php echo $row['name'];?></td>
+        <td><?php echo $row['continent']; ?></td>
+        <td><?php echo $row['independence_year']; ?></td>
+        <td><?php echo $row['head_of_state']; ?></td>
+      </tr>
 <?php endforeach; ?>
-</ul>
+</tbody>
+</table>
+
+  
+  
+

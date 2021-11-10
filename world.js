9f0c2c58d9a0ee46af0ce8c7746f853db3bd3c6e
Jamaal Henry <jamaalhenry30@gmail.com>
2021-11-09 22:57:40 -0500

    Edited all files to attain full functionality and display cities and countries listed

--- world.js
+++ world.js
@@ -1,27 +1,51 @@
-window.onload = function(){
-    let getBtn = document.getElementById("lookup");
-    let result = document.getElementById("result");
-    let input = document.getElementById("country")
+window.onload = function (){
 
-    getBtn.addEventListener('click', showSuggestion);
+    let input = document.getElementById('country')
+    let write = document.getElementById('result')
+    let Btn1= document.getElementById("lookup");
+    let Btn2 = document.getElementById("city");
 
-    function showSuggestion(e){
-        e.preventDefault();
-        var xhr = new XMLHttpRequest();
+    function clicker(e){
+        
+        const xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function(){
             let z = this.status;
             let rs = this.readyState;
             if(z == 200 && rs ==4){
-                result.innerHTML = this.responseText;
+                write.innerHTML = this.responseText;
             }
         }
-        let value = input.value;
-        xhr.open("GET","http://localhost/info2180-lab5/world.php?country="+value)
+        country = sanitizeString(input.value); 
+        context = 'countries';
+        xhr.open("GET", "http://localhost/info2180-lab5/world.php?country="+country+"&context="+context);
         xhr.send();
     }
+    function clicker2(e){
+    
+        const xhr = new XMLHttpRequest();
 
+        xhr.onreadystatechange = function(){
+            let z = this.status;
+            let rs = this.readyState;
+            if(z == 200 && rs ==4){
+                write.innerHTML = this.responseText;
+            }
+        }
+        country = sanitizeString(input.value); 
+        var context = "";
+        xhr.open("GET", "http://localhost/info2180-lab5/world.php?country="+country+"&context="+context);
+        xhr.send();
+        
 
+    }
 
+    Btn1.addEventListener("click",clicker);
+    Btn2.addEventListener("click",clicker2);
 
+    function sanitizeString(str){
+        str = str.replace(/[^a-z0-9áéíóúñü_-\s\.,]/gim,"");
+        return str.trim();
+        }
+    
 }
--- world.php
+++ world.php
@@ -5,9 +5,16 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
+$arraylist = array();
+$context = $_GET['context'];
 $count = 0;
+$error  = 'Enrty not valid';
 
-
+if(isset($country)){
+  $count = 1;
+}else{
+  $count = 2;
+}
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
@@ -16,8 +23,14 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<table> 
-<thead> 
+<table >
+
+<?php if($context=="countries"){ 
+  
+  if(count($results)<1){
+    echo($error);
+  }else{ ?>
+   <thead> 
     <tr>
       <th><?= 'Name'; ?></th>
       <th><?= 'Continent'; ?></th>
@@ -25,18 +38,52 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <th><?= 'Head of State'; ?></th>
     </tr>
   </thead>
-<tbody>
-<?php foreach ($results as $row): ?>
-      <tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?php echo $row['name'];?></td>
+      <td><?php echo $row['continent']; ?></td>
+      <td><?php echo $row['independence_year']; ?></td>
+      <td><?php echo $row['head_of_state']; ?></td>
+    </tr>
+  <?php endforeach;}  
+
+}else{
+
+  if(count($results)<1){
+    echo($error);
+  }else{
+
+    foreach($results as $data){
+      array_push($arraylist,$data['code']);
+    }
+  
+    ?>
+  
+  <thead> 
+    <tr>
+      <th><?= 'Name'; ?></th>
+      <th><?= 'Distrcit'; ?></th>
+      <th><?= 'Population'; ?></th>
+      
+    </tr>
+  </thead>
+
+
+      <?php
+      foreach($arraylist as $var){
+
+      $stmt2 = $conn->query("SELECT * FROM cities WHERE country_code LIKE '%$var%'"); 
+      $Results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+      ?>
+      <?php foreach ($Results2 as $row): ?>
+        <tr>
         <td><?php echo $row['name'];?></td>
-        <td><?php echo $row['continent']; ?></td>
-        <td><?php echo $row['independence_year']; ?></td>
-        <td><?php echo $row['head_of_state']; ?></td>
-      </tr>
-<?php endforeach; ?>
-</tbody>
+        <td><?php echo $row['district']; ?></td>
+        <td><?php echo $row['population']; ?></td>
+        </tr>
+      <?php endforeach; 
+    }
+  }
+}?>
+  
 </table>
-
-  
-  
-
